70f269d058da17e5a26eec9af5d8986ff6a0fe83
Pranjal-jadhav2010 <pranjal.jadhav031@gmail.com>
2026-06-03 16:44:53 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import random
+number=random.randint(1,100)
+guess=0
+while guess!=number:
+    guess=int(input("Guess a number(1-100):"))
+    if guess<number:
+        print("Too low!")
+    elif guess>number:
+        print("Too high!")
+    else:
+        print("Correct!")
